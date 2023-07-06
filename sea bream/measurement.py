9d75c4411cfa089e0bc96f5366915d60ad8ed762
fin358 <134633411+fin358@users.noreply.github.com>
2023-07-07 02:15:38 +0900

    Sea bream Measurement

--- sea bream/measurement.py
+++ sea bream/measurement.py
@@ -38,7 +38,7 @@ def color_coords(b, g, r):
 
 # 画像を読み込む
 # Load image
-img = cv2.imread('input.tif')
+img = cv2.imread('measurement_before.tif')
 if img is None:
     print("File could not be loaded")
 
@@ -59,28 +59,28 @@ blue_dist = np.linalg.norm(np.array(blue_coords[0])-np.array(blue_coords[1]))
 upper_dist = np.linalg.norm(np.array(yellow_coords[0])-np.array(cyan_coords[0]))
 lower_dist = np.linalg.norm(np.array(yellow_coords[0])-np.array(cyan_coords[1]))
 
-# 体長(length)を1としたときの体長(height)の比率を求める
-# Find the ratio of body length (height) to body length (length) as 1
+# 体長(横)を1としたときの体長(縦)の比率を求める
+# Find the ratio of body length (height) to body length (width) as 1
 raito1 = yellow_dist / blue_dist
 
-# 体長(height)を1としたときの胸鰭の幅の比率を求める
+# 体長(縦)を1としたときの胸鰭の幅の比率を求める
 # Find the ratio of the width of the pectoral fins when the body length (height) is 1
 raito2 = cyan_dist / yellow_dist
 
-# 体長(height)を1としたときの胸鰭(上)の幅の比率を求める
+# 体長(縦)を1としたときの胸鰭(上)の幅の比率を求める
 # Find the ratio of the width of the pectoral fin (upper) to the body length (height) as 1
 raito3 = upper_dist / yellow_dist
 
-# 体長(height)を1としたときの胸鰭(下)の幅の比率を求める
+# 体長(縦)を1としたときの胸鰭(下)の幅の比率を求める
 # Find the ratio of the width of the pectoral fin (lower) to the body length (height) as 1
 raito4 = lower_dist / yellow_dist
 
 # 結果を表示する
 # Show results
-print("体長(length)を1としたときの体長(height)の比率:", round(raito1,3))
-print("体長(height)を1としたときの胸鰭の幅の比率:", round(raito2,3))
-print("体長(height)を1としたときの胸鰭(上)の幅の比率:", round(raito3,3))
-print("体長(height)を1としたときの胸鰭(下)の幅の比率:", round(raito4,3))
+print("体長(横)を1としたときの体長(縦)の比率:", round(raito1,3))
+print("体長(縦)を1としたときの胸鰭の幅の比率:", round(raito2,3))
+print("体長(縦)を1としたときの胸鰭(上)の幅の比率:", round(raito3,3))
+print("体長(縦)を1としたときの胸鰭(下)の幅の比率:", round(raito4,3))
 
 # 画像を表示する
 # Show Image
